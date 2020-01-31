702539284f7d2907d8619996efc8da4e8cff42d4
hectorvillarreal34 <60371860+hectorvillarreal34@users.noreply.github.com>
2020-01-31 15:18:09 -0700

    Add files via upload

---
+++ MiPrimerTrabajoConsola/MiPrimerTrabajoConsola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerTrabajoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("introduce un numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("introduce un numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+
+            Console.WriteLine("la suma de los numeros introducidos es: " + c);
+            Console.ReadKey();
+        }
+    }
+}
